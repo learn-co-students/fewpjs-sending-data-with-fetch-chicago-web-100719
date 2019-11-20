129dbf47737d69a0ce4f4db447e2e53ae26d7dce
benjewman <bennyadave@gmail.com>
2019-11-20 17:13:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    const body = document.querySelector('body')
+    let formData = {
+        name,
+        email
+    }
+    let postObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    };
+    return fetch('http://localhost:3000/users', postObj)
+        .then(resp => resp.json())
+        .then(user => document.body.innerHTML = user['id'])
+        .catch(error => document.body.innerHTML = error.message)
+}
