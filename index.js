0b12e7d9e37b81a1ddb0ce8fb2413cb12d62b63b
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-11-20 16:59:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then(response => response.json())
+    .then(returndata => {
+        document.body.innerHTML = returndata.id
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
+
