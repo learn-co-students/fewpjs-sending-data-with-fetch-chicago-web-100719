adb6033f672946a271d2a25fea838f8a6cee0dfd
github NikolaCzv <github nikola.raicic@hotmail.com>
2019-11-22 09:21:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', configurationObject(name, email))
+    .then(resp => resp.json())
+    .then(object => {
+        document.body.innerHTML = object["id"]
+    })
+    .catch(error => { 
+        document.body.innerHTML = error.message
+    })
+}
+
+function configurationObject(name, email)  {
+    return {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+            },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    }
+};
