71b04b6973859d88dfa14ded55a5760a3af76837
Daniel Ionita <danielionita88@yahoo.com>
2019-11-23 09:23:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+    const body = document.querySelector('body')
+    
+    let formData = {
+        name,
+        email
+    }
+    
+    let confObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    
+    return fetch('http://localhost:3000/users', confObj)
+     .then(resp => resp.json())
+     .then(obj => document.body.innerHTML = obj['id'])
+     .catch(error => document.body.innerHTML = error.message)
+    
+}
